c56f34090eea3fffd5768f0c6513f0b6007590d5
chenPearch <45401724+chenPearch@users.noreply.github.com>
2020-11-24 12:57:32 +0200

    added veriables for CWD, the taget folder and the raspberry pi's ip. that clean up the code a bit

--- Upload.bat
+++ Upload.bat
@@ -4,6 +4,15 @@ rem this file uploads the
 rem Main.py and opencvBasic.py files
 rem on to the raspberry pi
 echo uploding 
-pscp -pw root C:\Users\chen7\Documents\FRC\visionprossing\code\helperFile.py pi@10.45.86.69:/home/pi/frcvision
-pscp -pw root C:\Users\chen7\Documents\FRC\visionprossing\code\Main.py pi@10.45.86.69:/home/pi/frcvision
-pscp -pw root C:\Users\chen7\Documents\FRC\visionprossing\code\HSVdata.json pi@10.45.86.69:/home/pi/frcvision
+rem 
+rem 10.45.86.69 - the raspberry pi's ip in the local network
+rem C:\Users\chen7\Documents\FRC\visionprossing\code\... - the path to the uploaded file
+rem /home/pi/frcvision - the target folder in the Raspberry pi
+rem 
+SET _CWD=C:\Users\chen7\Documents\FRC\visionprossing\code
+SET _PI_IP=10.45.86.69
+SET _TARGET_FOLDER=/home/pi/frcvision
+pscp -pw root %_CWD%\helperFile.py pi@%_PI_IP%:%_TARGET_FOLDER%
+pscp -pw root %_CWD%\Main.py pi@%_PI_IP%:%_TARGET_FOLDER%
+pscp -pw root %_CWD%\ HSVdata.json pi@%_PI_IP%:%_TARGET_FOLDER%
+echo finished uploading
